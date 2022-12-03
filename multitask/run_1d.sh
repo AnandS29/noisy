e0cefe1ef70e65da15ac4e2280b5f74e20209067
Anand <anandsranjan@berkeley.edu>
2022-12-02 19:27:13 -0800

    pre merge

--- multitask/run_1d.sh
+++ multitask/run_1d.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Linear 1D
 # Learning from preferences
 # Save stats
@@ -5,4 +6,8 @@
 # 25k timesteps
 # 90k comparisons
 
-python3 test_pref.py --env linear1d --pref --stats --noise --verbose --timesteps 25000 --comparisons 90000
+cd /data/noisy 
+pip install sb3-contrib
+pip install -e .
+cd multitask
+python3 test_pref.py --env linear1d --pref --stats --noise --verbose --timesteps 25000 --comparisons 90000 --algo trpo
